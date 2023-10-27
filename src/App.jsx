28b685222b4daedd5e68e03dd890836e11b9c3e3
Aada Härmä <aadaharma@Aadas-MacBook-Air.local>
2023-10-27 14:11:31 +0300

    Styling tests

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
       <Navbar />
       <div id='content' className='content'>
       <section id= 'info' className='info'>
-          <h1>Important info</h1>
+          <h1>Hi, I am Aada. Nice to meet you!</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </section>
         <section id='exp'>
