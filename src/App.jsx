c8876363f2600928f310df621db6eaedfbca5dc7
Aada Härmä <aadaharma@Aadas-MacBook-Air.local>
2024-11-12 15:45:40 +0200

    Add scrollHandler

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Navbar from './components/navbar/navbar';
 
 export default function App() {
+  const handleScroll = (e) => {
+    const scrollDown = e.deltaY > 0;
+
+    const sections = document.querySelectorAll('section');
+    const currentSectionIndex = Array.from(sections).findIndex((section) => {
+      const rect = section.getBoundingClientRect();
+      return rect.top <= window.innerHeight && rect.bottom > 0;
+    });
+
+    let nextSectionIndex = currentSectionIndex;
+    if (scrollDown) {
+      nextSectionIndex = Math.min(currentSectionIndex + 1, sections.length - 1);
+    } else {
+      nextSectionIndex = Math.max(currentSectionIndex - 1, 0);
+    }
+
+    // Scroll to the next or previous section
+    sections[nextSectionIndex].scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    e.preventDefault(); // Prevent default scroll behavior
+
+    };
+  
+
   return (
     <>
     <main>
@@ -10,6 +38,9 @@ export default function App() {
       <div id='content' className='content'>
       <section id = 'start' className = 'start'>
       <img className='hello' src='/feathered_ohhello.png' alt='text: Oh, Hello!'></img>
+      <aside id= 'photo'>
+          <img className='portrait' src='/resumepic.JPG' alt='portrait of Aada'></img>
+      </aside>
       </section>
       <section id= 'info' className='info'>
           <h1>Hi, I am Aada. Nice to meet you!</h1>
@@ -22,9 +53,7 @@ export default function App() {
           <h1>My work experience</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d</p>
         </section>
-        <aside id= 'photo'>
-          <img className='portrait' src='/resumepic.JPG' alt='portrait of Aada'></img>
-        </aside>
+        
         <aside id='expdiag'>
           <p>Here maybe experience circle diagrams</p>
         </aside>
